368e995a2402fcf7372e1b6001a87384c36cd9cb
sanikommu-bhanu <sanikommu.bhanu@gmail.com>
2026-04-16 17:12:41 +0530

    Update tsconfig target

--- features/auth/PinLock.tsx
+++ features/auth/PinLock.tsx
@@ -16,10 +16,10 @@ export function PinLock() {
   const [attempts, setAttempts] = useState(0)
   const [shaking, setShaking] = useState(false)
 
-  const maxPinLength = 4
+  const maxPinLength = 6
 
   useEffect(() => {
-   if (pin.length === maxPinLength && !shaking) { 
+   if (pin.length === 4 && !shaking) { 
       handleVerify()
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
